a2853a107695c73879ae1e748aacd71d30a540a6
WebNaresh <nareshbhosale73@gmail.com>
2022-01-07 19:01:45 +0530

    hi

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,39 +4,49 @@ import React from 'react'
 
 export default function TextForm(props) {
 
-    const [text,setText]= useState('')
+    const [text, setText] = useState('')
 
-    const handleClick=()=>{
+    const handleClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
     }
 
-    const handleLOClick=()=>{
+    const handleLOClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
     }
-    const handleOnChange=(e)=>{
+    const handleClearCLick = ()=>{
+        let newText = "";
+        setText(newText);
+    }
+    const handleCopyCLick = () =>{
+        navigator.clipboard.writeText(text)
+        console.log(text);
+    }
+    const handleOnChange = (e) => {
         setText(e.target.value)
     }
 
     return (
         <>
-        <div className="container">
-            <div className="container my-3">
-                <h1>{props.heading}</h1>
-                <div className="mb-3 my-3">
-                    <textarea className="form-control" value={ text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <div className="container">
+                <div className="container my-3">
+                    <h1>{props.heading}</h1>
+                    <div className="mb-3 my-3">
+                        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    </div>
+                    <button className="btn btn-primary mx-1" onClick={handleClick}>Convert to Uppercase</button>
+                    <button className="btn btn-primary mx-1" onClick={handleLOClick}>Convert to Lowercase</button>
+                    <button className="btn btn-primary mx-1" onClick={handleClearCLick}>Clear Content</button>
+                    <button className="btn btn-primary mx-1" onClick={handleCopyCLick}>Copy Content</button>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleLOClick}>Convert to Lowercase</button>
-            </div>
             </div>
             <div className="container my-2">
                 <h1>Your text Summary</h1>
                 <p> {text.split("").length} words and {text.length} characters</p>
                 <p> {0.008 * text.split("").length} minutes read</p>
-                <h2>Previes</h2>
-                <p>{text}</p>
+                <h1>Previes</h1>
+                <p className="text-break">{text}</p>
             </div>
         </>
     )
