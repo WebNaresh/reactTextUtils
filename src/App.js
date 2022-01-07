56ee9daefc9864fb3fe0c5c308f9e22078d8c1f1
WebNaresh <nareshbhosale73@gmail.com>
2022-01-07 19:03:28 +0530

    hi3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import './custom.css'
 
-let name = "harry";
 
 function App() {
   return (
     <>
-      <Navbar title='TextUtils' firstlink='About' ></Navbar>
+      <Navbar  title='TextUtils' firstlink='About' ></Navbar>
       <TextForm heading="Enter Text here"></TextForm>
+      {/* <About/> */}
     </>
   );
 }
